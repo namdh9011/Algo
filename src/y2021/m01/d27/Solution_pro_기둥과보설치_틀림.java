4bc8535dfe81e35cce707e26b42f03d971e695d4
남동해 <namdh0911@gmail.com>
2021-02-03 13:32:04 +0900

    d27 제목수정

--- src/y2021/m01/d27/Solution_pro_기둥과보설치_틀림.java
+++ src/y2021/m01/d27/Solution_pro_기둥과보설치_틀림.java
@@ -12,7 +12,7 @@ import java.util.PriorityQueue;
 - 
 */
 
-public class Solution_pro_기둥과보설치 {
+public class Solution_pro_기둥과보설치_틀림 {
 	public static void main(String[] args) throws Exception {
 //		int n = 5;
 //		int build_frame[][] = {{1,0,0,1},{1,1,1,1},{2,1,0,1},{2,2,1,1},{5,0,0,1},{5,1,0,1},{4,2,1,1},{3,2,1,1}};
