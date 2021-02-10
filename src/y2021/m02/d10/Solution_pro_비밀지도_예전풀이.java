c62d5df923905c33ed4b53e53ecc74999b2ca4ef
남동해 <namdh0911@gmail.com>
2021-02-10 16:55:15 +0900

    문자열, 구현 예전풀이

---
+++ src/y2021/m02/d10/Solution_pro_비밀지도_예전풀이.java
@@ -0,0 +1,55 @@
+package y2021.m02.d10;
+
+import java.util.Arrays;
+
+public class Solution_pro_비밀지도_예전풀이 {
+	public static void main(String[] args) throws Exception {
+		int n = 5;
+		int[] arr1 = {9, 20, 28, 18, 11};
+		int[] arr2 = {30, 1, 21, 17, 28};
+		
+		System.out.println(Arrays.toString(solution(n,arr1,arr2)));
+	}
+	public static String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+        String[] temp1 = new String[n];
+        String[] temp2 = new String[n];
+        
+        for(int i=0;i<n;i++) {
+        	temp1[i] = Integer.toBinaryString(arr1[i]);
+        	int t = temp1[i].length();
+        	if(t<n) {
+        		for(int j=0;j<n-t;j++) {
+        			temp1[i]="0"+temp1[i];
+        		}
+        	}
+        	temp2[i] = Integer.toBinaryString(arr2[i]);
+        	t = temp2[i].length();
+        	if(t<n) {
+        		for(int j=0;j<n-t;j++) {
+        			temp2[i]="0"+temp2[i];
+        		}
+        	}
+        }
+        System.out.println(Arrays.toString(temp1));
+        System.out.println(Arrays.toString(temp2));
+        for(int i=0;i<n;i++) {
+        	answer[i] = change(temp1[i],temp2[i]);
+        }
+        
+        return answer;
+    }
+
+	private static String change(String string, String string2) {
+		int n = string.length();
+		String result = "";
+		for(int i=0;i<n;i++) {
+			if(string.charAt(i)=='0'&&string2.charAt(i)=='0') {
+				result = result+" ";
+			}else {
+				result = result+"#";
+			}
+		}
+		return result;
+	}
+}
