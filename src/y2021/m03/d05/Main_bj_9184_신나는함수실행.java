b7152523ee5ae7cca9567c700c035b78e4b4de62
남동해 <namdh0911@gmail.com>
2021-03-05 23:48:00 +0900

    dp, 재귀

--- src/y2021/m03/d05/Main_bj_9184_신나는함수실행.java
+++ src/y2021/m03/d05/Main_bj_9184_신나는함수실행.java
@@ -10,11 +10,13 @@ import java.util.StringTokenizer;
 10 4 6
 50 50 50
 -1 7 18
+1 1 21
 -1 -1 -1
  */
 
 
 class Main_bj_9184_신나는함수실행 {
+	public static int[][][] dp;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
@@ -22,12 +24,15 @@ class Main_bj_9184_신나는함수실행 {
 		int b = 0;
 		int c = 0;
 		StringBuilder sb = new StringBuilder();
-		while(a!=-1&&b!=-1&&c!=-1) {
+		dp = new int[21][21][21];
+		while(true) {
 			st = new StringTokenizer(br.readLine());
 			a = Integer.parseInt(st.nextToken());
 			b = Integer.parseInt(st.nextToken());
 			c = Integer.parseInt(st.nextToken());
-			System.out.println(w(a,b,c));
+			if(a==-1&&b==-1&&c==-1) {
+				break;
+			}
 			sb.append("w(").append(a).append(", ").append(b).append(", ").append(c).append(") = ").append(w(a,b,c)).append("\n");
 		}
 		System.out.println(sb.toString());
@@ -38,12 +43,15 @@ class Main_bj_9184_신나는함수실행 {
 			return 1;
 		}
 		if(a>20||b>20||c>20) {
-			return w(20,20,20);
+			return dp[20][20][20]=w(20,20,20);
+		}
+		if(dp[a][b][c]!=0) {
+			return dp[a][b][c];
 		}
 		if(a<b&&b<c) {
-			return w(a,b,c-1)+w(a,b-1,c-1)-w(a,b-1,c);
+			return dp[a][b][c] = w(a,b,c-1)+w(a,b-1,c-1)-w(a,b-1,c);
 		}else {
-			return w(a-1,b,c)+w(a-1,b-1,c)+w(a-1,b,c-1)-w(a-1,b-1,c-1);
+			return dp[a][b][c] = w(a-1,b,c)+w(a-1,b-1,c)+w(a-1,b,c-1)-w(a-1,b-1,c-1);
 		}
 	}
 }
