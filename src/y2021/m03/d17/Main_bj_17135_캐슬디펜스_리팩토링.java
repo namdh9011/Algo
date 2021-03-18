f6a5e9589a3ff90b02b5454b418e7db68a51d270
남동해 <namdh0911@gmail.com>
2021-03-19 05:12:18 +0900

    algo: 일부수정

--- src/y2021/m03/d17/Main_bj_17135_캐슬디펜스_리팩토링.java
+++ src/y2021/m03/d17/Main_bj_17135_캐슬디펜스_리팩토링.java
@@ -1,97 +1,61 @@
 package y2021.m03.d17;
 
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 /*
-6 5 1
-1 0 1 0 1
-0 1 0 1 0
-1 1 0 0 0
-0 0 0 1 1
-1 1 0 1 1
-0 0 1 0 0
+7 8
+0 1 3
+1 1 7
+0 7 6
+1 7 1
+0 3 7
+0 4 2
+0 1 1
+1 1 1
  */
-public class Main_bj_17135_캐슬디펜스_리팩토링 {
-	public static int map[][],N,M,D,di[] ={0,-1,0},dj[]={-1,0,1},answer;
+
+class Main_bj_17135_캐슬디펜스_리팩토링 {
+	public static int[] arr;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		D = Integer.parseInt(st.nextToken());
-		map = new int[N][M];
-		for(int i=0;i<N;i++) {
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		arr = new int[N+1];
+		for(int i=0;i<=N;i++) {
+			arr[i]=i;
+		}
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<M;i++) {
 			st = new StringTokenizer(br.readLine());
-			for(int j=0;j<M;j++) {
-				map[i][j]=Integer.parseInt(st.nextToken());
+			int com = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			if(com==0) {
+				union(a,b);
+			}else {
+				a = find(a);
+				b = find(b);
+				String s = (a==b)?"YES":"NO";
+				sb.append(s).append("\n");
 			}
 		}
-		int[] arr = new int[3];
-		boolean[] visit = new boolean[M];
-		comb(arr, 0, 0, visit);
-		System.out.println(answer);
+		System.out.println(sb.toString());
 	}
-	public static void comb(int[] arr, int count, int start, boolean[] visit) {
-		if(count==3) {
-			int dead = defence(arr);		//arr = archer의 위치
-			if(answer<dead) {				//죽은 숫자 갱신
-				answer=dead;
-			}
-			return;
-		}
-		for(int i=start;i<M;i++) {
-			if(!visit[i]) {
-				visit[i]=true;
-				arr[count]=i;
-				comb(arr,count+1,i,visit);
-				visit[i]=false;
-			}
+	public static void union(int a, int b) {
+		a = find(a);
+		b = find(b);
+		if(a!=b) {
+			arr[b]=a;
 		}
 	}
-	public static int defence(int[] archer) {		
-		int dead = 0;
-		int[][] tmap = new int[N][M];
-		for(int i=0;i<tmap.length;i++) {
-			tmap[i]=map[i].clone();
+	public static int find(int x) {
+		if(x==arr[x]) {
+			return x;
+		}else {
+			return arr[x]=find(arr[x]);
 		}
-		for(int line=N-1;line>=0;line--) {
-			Queue<int[]> remove = new LinkedList<>();
-			for(int i=0;i<archer.length;i++) {
-				int arc = archer[i];
-				if(tmap[line][arc]==1) {					//궁수 바로 위칸이 1이면
-					remove.add(new int[] {line, arc});
-				}else {										//궁수 바로 위칸이 0이면
-					if(D>1) {								//사거리가 1보다 길때
-						boolean[][] visit = new boolean[N][M];
-						Queue<int[]> q = new LinkedList<>();
-						q.add(new int[] {line, arc, 1});
-						visit[line][arc]=true;
-						here:while(!q.isEmpty()) {
-							int[] curr = q.poll();
-							for(int d=0;d<3;d++) {
-								int ni = curr[0]+di[d];
-								int nj = curr[1]+dj[d];
-								if(ni>=0&&nj>=0&&ni<N&&nj<M&&!visit[ni][nj]&&curr[2]+1<=D) {
-									visit[ni][nj]=true;
-									q.add(new int[] {ni,nj,curr[2]+1});
-									if(tmap[ni][nj]==1) {		//제거 대상을 만날 경우
-										remove.add(new int[] {ni,nj,curr[2]+1});
-										break here;
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			while(!remove.isEmpty()) {
-				int[] curr = remove.poll();
-				if(tmap[curr[0]][curr[1]]==1) {		//제거 대상이 겹치지 않을때만 제거해주고 죽은 수를 늘려줌
-					tmap[curr[0]][curr[1]]=0;
-					dead++;
-				}
-			}
-		}
-		return dead;
 	}
 }
