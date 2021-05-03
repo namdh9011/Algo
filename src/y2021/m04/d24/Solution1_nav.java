c0fc16c71f523df6891af461128e879e160ebd68
남동해 <namdh0911@gmail.com>
2021-05-03 23:21:59 +0900

    edit: 내용수정

--- src/y2021/m04/d24/Solution1_nav.java
+++ src/y2021/m04/d24/Solution1_nav.java
@@ -2,33 +2,30 @@ package y2021.m04.d24;
 
 public class Solution1_nav {
 	public static void main(String[] args) {
-		int lottos[] = {44,1,0,0,31,25};
-		int win_nums[] = {31,10,45,1,6,19};
-		System.out.println(solution(lottos,win_nums));
+		String m = "kkaxbycyz";
+		String k = "abc";
+		System.out.println(solution(m,k));
 	}
 
-	private static int[] solution(int[] lottos, int[] win_nums) {
-		int max = 0;
-        int min = 0;
-        boolean[] win = new boolean[47];
-        for(int i=0;i<win_nums.length;i++){
-            win[win_nums[i]]=true;
-        }
-        for(int i=0;i<lottos.length;i++){
-            int tar = lottos[i];
-            if(tar==0){
-                max++;
-            }else{
-                if(win[tar]){
-                    min++;
-                }
-            }
-        }
-        max+=min;
-        int[] map = {6,6,5,4,3,2,1};
-        int[] answer = new int[2];
-        answer[0]=map[max];
-        answer[1]=map[min];
-        return answer;
+	private static String solution(String m, String k) {
+		int index = 0;
+		int size = k.length();
+		char tar = k.charAt(index);
+		StringBuilder sb = new StringBuilder();
+		for(int i=0;i<m.length();i++) {
+			if(m.charAt(i)!=tar) {
+				sb.append(m.charAt(i));
+			}else {
+				index++;
+				if(index>=size) {
+					index=i;
+					break;
+				}
+				tar=k.charAt(index);
+			}
+		}
+		System.out.println(m.substring(index+1, m.length()));
+		sb.append(m.substring(index+1, m.length()));
+		return sb.toString();
 	}
 }
