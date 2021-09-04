e5d5d14ac13e927d6319d86f56355de96c0bd6d3
남동해 <namdh0911@gmail.com>
2021-09-04 23:35:41 +0900

    edit: 파일 생성 오류수정

--- src/y2021/m09/d03/Main_bj_10026_적록색약.java
+++ src/y2021/m09/d03/Main_bj_10026_적록색약.java
@@ -2,69 +2,62 @@ package y2021.m09.d03;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 /*
-메모리 : 12136 KB
-시간 : 96 ms
+메모리 : 12232 KB
+시간 : 964 ms
 
-최적 메모리 : 13524 KB
-최적 시간 : 76 ms
+최적 메모리 : 12040 KB
+최적 시간 : 124 ms
+
+- 백트래킹 해줘야 할듯.
 */
 /*
-5
-RRRBB
-GGBBB
-BBBRR
-BBRRR
-RRRRR
+5 5
+IEFCJ
+FHFKC
+FFALF
+HFGCF
+HMCHH
  */
 public class Main_bj_10026_적록색약 {
-	public static int N,di[]= {-1,0,1,0}, dj[]= {0,-1,0,1}, res, resDif;
-	public static boolean visit[][];
+	public static int R, C, di[] = {-1,1,0,0}, dj[]= {0,0,-1,1}, max;
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		N = Integer.parseInt(br.readLine());
-		char[][] map = new char[N][N];
-		char[][] mapDif = new char[N][N];
-		visit = new boolean[N][N];
-		res=0;
-		for(int i=0;i<N;i++) {
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		boolean[] visit = new boolean[26];
+		max = Integer.MIN_VALUE;
+		
+		char[][] board = new char[R][C];
+		for(int i=0;i<R;i++) {
 			String input = br.readLine();
-			for(int j=0;j<N;j++) {
-				map[i][j]=input.charAt(j);
-				mapDif[i][j]=input.charAt(j);
-				if(mapDif[i][j]=='R') {
-					mapDif[i][j]='G';
-				}
+			for(int j=0;j<C;j++) {
+				board[i][j]=input.charAt(j);
 			}
 		}
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				if(!visit[i][j]) {
-					visit[i][j]=true;
-					res++;
-					dfs(i,j,map[i][j],map);
-				}
-			}
-		}
-		visit = new boolean[N][N];
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<N;j++) {
-				if(!visit[i][j]) {
-					visit[i][j]=true;
-					resDif++;
-					dfs(i,j,mapDif[i][j],mapDif);
-				}
-			}
-		}
-		System.out.println(res + " " + resDif);
+		
+		char tar = board[0][0];
+		visit[tar-'A']=true;
+		dfs(0,0,board,visit,1);
+		System.out.println(max);
 	}
-	private static void dfs(int x, int y, char tar,char[][] map) {
+
+	private static void dfs(int i, int j, char[][] board, boolean[] visit, int count) {
+		if(max<count) {
+			max=count;
+		}
 		for(int d=0;d<4;d++) {
-			int ni = x + di[d];
-			int nj = y + dj[d];
-			if(ni>=0&&nj>=0&&ni<N&&nj<N&&!visit[ni][nj]&&map[ni][nj]==tar) {
-				visit[ni][nj]=true;
-				dfs(ni,nj,tar,map);
+			int ni = i + di[d];
+			int nj = j + dj[d];
+			if(ni>=0&&nj>=0&&ni<R&&nj<C) {
+				int tar = board[ni][nj];
+				if(!visit[tar-'A']) {
+					visit[tar-'A']=true;
+					dfs(ni,nj,board,visit, count+1);
+					visit[tar-'A']=false;
+				}
 			}
 		}
 	}
