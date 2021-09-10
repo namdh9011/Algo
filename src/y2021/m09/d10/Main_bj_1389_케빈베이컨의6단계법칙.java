38cfe04e375393609bdb611d044479f2e2b7bd6d
남동해 <namdh0911@gmail.com>
2021-09-10 16:15:09 +0900

    algo: init

---
+++ src/y2021/m09/d10/Main_bj_1389_케빈베이컨의6단계법칙.java
@@ -0,0 +1,30 @@
+package y2021.m09.d10;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+/*
+메모리 : 11528 KB
+시간 : 80 ms
+
+최적 메모리 : 12740 KB
+최적 시간 : 68 ms
+
+*/
+/*
+5 5
+1 3
+1 4
+4 5
+4 3
+3 2
+ */
+public class Main_bj_1389_케빈베이컨의6단계법칙 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+	}
+	
+}
+
+
